020e112b06ee0e0ea999d53064d0f58830159e77
Glad <iamgladwin@gmail.com>
2024-04-06 00:20:16 -0400

    import jwt liibrary

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const userService = require("./user-service.js");
 const passport = require("passport");
+const passportJwt = require("passport-jwt");
 
 const HTTP_PORT = process.env.PORT || 8080;
 
@@ -12,15 +13,15 @@ app.use(express.json());
 app.use(cors());
 
 // set up
-let ExtractJwt = passport.ExtractJwt;
-let JwtStrategy = passport.Strategy;
+const ExtractJwt = passportJwt.ExtractJwt;
+const JwtStrategy = passportJwt.Strategy;
 
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET, // Use secret from environment variable
 };
 
-let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   console.log("payload received", jwt_payload);
 
   if (jwt_payload) {
@@ -42,102 +43,7 @@ passport.use(strategy);
 // add passport as application-level middleware
 app.use(passport.initialize());
 
-app.post("/api/user/register", (req, res) => {
-  userService
-    .registerUser(req.body)
-    .then((msg) => {
-      res.json({ message: msg });
-    })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
-});
-
-app.post("/api/user/login", (req, res) => {
-  userService
-    .checkUser(req.body)
-    .then((user) => {
-      let payload = {
-        _id: user._id,
-        userName: user.userName,
-      };
-
-      let token = jwt.sign(payload, process.env.JWT_SECRET); // Sign with secret from environment
-
-      res.json({ message: "login successful", token: token });
-    })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
-});
-
-const authenticate = passport.authenticate("jwt", { session: false });
-
-app.get("/api/user/favourites", authenticate, (req, res) => {
-  userService
-    .getFavourites(req.user._id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
-
-app.put("/api/user/favourites/:id", authenticate, (req, res) => {
-  userService
-    .addFavourite(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
-
-app.delete("/api/user/favourites/:id", authenticate, (req, res) => {
-  userService
-    .removeFavourite(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
-
-app.get("/api/user/history", authenticate, (req, res) => {
-  userService
-    .getHistory(req.user._id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
-
-app.put("/api/user/history/:id", authenticate, (req, res) => {
-  userService
-    .addHistory(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
-
-app.delete("/api/user/history/:id", authenticate, (req, res) => {
-  userService
-    .removeHistory(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
-});
+// Your routes...
 
 userService
   .connect()
